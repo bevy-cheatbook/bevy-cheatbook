0ca56fa16d0b3ab309724ec0850c6846999f92d7
Miren <mirenbhakta21@gmail.com>
2021-02-22 09:00:45 -0600

    rename zoom to radius removed reference height magic number and use camera fov for making panning independent instead removed PanOrbitCamera::rotation field because using the entity's Transform::rotation has the same behavior simplify the transform matrix math simplify the example spawn_camera function spawn_scene now just calls the spawn_camera example function

--- src/code_bevy_release/examples/pan-orbit-camera.rs
+++ src/code_bevy_release/examples/pan-orbit-camera.rs
@@ -1,13 +1,13 @@
 use bevy::prelude::*;
 use bevy::input::mouse::{MouseWheel,MouseMotion};
+use bevy::render::camera::PerspectiveProjection;
 
 // ANCHOR: example
 /// Tags an entity as capable of panning and orbiting.
 struct PanOrbitCamera {
     /// The "focus point" to orbit around. It is automatically updated when panning the camera
     pub focus: Vec3,
-    pub zoom: f32,
-    pub rotation: Quat,
+    pub radius: f32,
     pub upside_down: bool,
 }
 
@@ -15,8 +15,7 @@ impl Default for PanOrbitCamera {
     fn default() -> Self {
         PanOrbitCamera {
             focus: Vec3::zero(),
-            zoom: 5.0,
-            rotation: Quat::identity(),
+            radius: 5.0,
             upside_down: false,
         }
     }
@@ -30,7 +29,7 @@ fn pan_orbit_camera(
     ev_motion: Res<Events<MouseMotion>>,
     ev_mouse: Res<Input<MouseButton>>,
     ev_scroll: Res<Events<MouseWheel>>,
-    mut query: Query<(&mut PanOrbitCamera, &mut Transform)>,
+    mut query: Query<(&mut PanOrbitCamera, &mut Transform, &PerspectiveProjection)>,
 ) {
     // change input mapping for orbit and panning here
     let orbit_button = MouseButton::Right;
@@ -58,7 +57,7 @@ fn pan_orbit_camera(
         orbit_button_changed = true;
     }
 
-    for (mut pan_orbit, mut transform) in query.iter_mut() {
+    for (mut pan_orbit, mut transform, projection) in query.iter_mut() {
         if orbit_button_changed {
             // only check for upside down when orbiting started or ended this frame
             // if the camera is "upside" down, panning horizontally would be inverted, so invert the input to make it correct
@@ -69,7 +68,6 @@ fn pan_orbit_camera(
         let mut any = false;
         if rotation_move.length_squared() > 0.0 {
             any = true;
-
             let window = get_primary_window_size(&windows);
             let delta_x = {
                 let delta = rotation_move.x / window.x * std::f32::consts::PI * 2.0;
@@ -78,38 +76,32 @@ fn pan_orbit_camera(
             let delta_y = rotation_move.y / window.y * std::f32::consts::PI;
             let yaw = Quat::from_rotation_y(-delta_x);
             let pitch = Quat::from_rotation_x(-delta_y);
-            pan_orbit.rotation = yaw * pan_orbit.rotation; // rotate around global y axis
-            pan_orbit.rotation = pan_orbit.rotation * pitch; // rotate around local x axis
+            transform.rotation = yaw * transform.rotation; // rotate around global y axis
+            transform.rotation = transform.rotation * pitch; // rotate around local x axis
         } else if pan.length_squared() > 0.0 {
             any = true;
-            // make panning distance independent of resolution,
+            // make panning distance independent of resolution and FOV,
             let window = get_primary_window_size(&windows);
-            let ref_height = 0.829;
-            let reference = Vec2::new(ref_height * window.x / window.y, ref_height);
-            let norm = reference / window;
-            pan *= norm;
+            pan *= Vec2::new(projection.fov * projection.aspect_ratio, projection.fov) / window;
             // translate by local axes
             let right = transform.rotation * Vec3::unit_x() * -pan.x;
             let up = transform.rotation * Vec3::unit_y() * pan.y;
             // make panning proportional to distance away from focus point
-            let translation = (right + up) * pan_orbit.zoom;
+            let translation = (right + up) * pan_orbit.radius;
             pan_orbit.focus += translation;
         } else if scroll.abs() > 0.0 {
             any = true;
-            pan_orbit.zoom -= scroll * pan_orbit.zoom * 0.2;
+            pan_orbit.radius -= scroll * pan_orbit.radius * 0.2;
             // dont allow zoom to reach zero or you get stuck
-            pan_orbit.zoom = f32::max(pan_orbit.zoom, 0.05);
+            pan_orbit.radius = f32::max(pan_orbit.radius, 0.05);
         }
 
         if any {
             // emulating parent/child to make the yaw/y-axis rotation behave like a turntable
             // parent = x and y rotation
             // child = z-offset
-            let parent = Mat4::from_rotation_translation(pan_orbit.rotation, pan_orbit.focus);
-            let child = Mat4::from_rotation_translation(Quat::identity(), Vec3::new(0.0, 0.0, pan_orbit.zoom));
-            let (_, rot, pos) = parent.mul_mat4(&child).to_scale_rotation_translation();
-            transform.translation = pos;
-            transform.rotation = rot;
+            let rot_matrix = Mat3::from_quat(transform.rotation);
+            transform.translation = pan_orbit.focus + rot_matrix.mul_vec3(Vec3::new(0.0, 0.0, pan_orbit.radius));
         }
     }
 }
@@ -122,19 +114,16 @@ fn get_primary_window_size(windows: &Res<Windows>) -> Vec2 {
 
 /// Spawn a camera like this
 fn spawn_camera(commands: &mut Commands) {
-    use bevy::render::camera::PerspectiveProjection;
+    let translation = Vec3::new(-2.0, 2.5, 5.0);
+    let radius = translation.length();
 
-    let camera = PanOrbitCamera::default();
-    let zoom = camera.zoom;
-
-    commands.spawn((camera, ))
-        .with_bundle(Camera3dBundle {
-            transform: Transform::from_translation(Vec3::new(0.0, 0.0, zoom))
-                .looking_at(Vec3::default(), Vec3::unit_y()),
-            perspective_projection: PerspectiveProjection {
-                near: 0.01, // no clue why the near clipping plane is set to 1.0 by default
-                ..Default::default()
-            },
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_translation(translation)
+            .looking_at(Vec3::zero(), Vec3::unit_y()),
+        ..Default::default()
+    })
+        .with(PanOrbitCamera {
+            radius,
             ..Default::default()
         });
 }
@@ -145,13 +134,7 @@ fn spawn_scene(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    use bevy::render::camera::PerspectiveProjection;
-
     // spawn a cube and a light
-    let transform = Transform::from_translation(Vec3::new(-2.0, 2.5, 5.0))
-        .looking_at(Vec3::default(), Vec3::unit_y());
-    let rotation = transform.rotation;
-    let zoom = transform.translation.length();
     commands
         .spawn(PbrBundle {
             mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
@@ -162,19 +145,9 @@ fn spawn_scene(
         .spawn(LightBundle {
             transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
             ..Default::default()
-        })
-        .spawn(Camera3dBundle {
-            transform,
-            perspective_projection: PerspectiveProjection {
-                near: 0.01, // no clue why the near clipping plane is set to 1.0 by default
-                ..Default::default()
-            },
-            ..Default::default()
-        }).with(PanOrbitCamera {
-            rotation,
-            zoom,
-            ..Default::default()
         });
+
+    spawn_camera(commands);
 }
 
 fn main() {
