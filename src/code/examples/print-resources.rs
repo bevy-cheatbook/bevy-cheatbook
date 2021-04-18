e1d9019bfac048cd2783d03294c48128573d419f
Mike Hsu <mike.hsu@gmail.com>
2021-04-18 13:26:47 -0700

    add example for printing resources

---
+++ src/code/examples/print-resources.rs
@@ -0,0 +1,29 @@
+use bevy::ecs::{archetype::Archetypes, component::Components};
+use bevy::prelude::*;
+use bevy::reflect::TypeRegistration;
+
+// ANCHOR: example
+fn print_resources(archetypes: &Archetypes, components: &Components) {
+    let mut r: Vec<String> = archetypes
+        .resource()
+        .components()
+        .map(|id| components.get_info(id).unwrap())
+        // get_short_name removes the path information
+        // i.e. `bevy_audio::audio::Audio` -> `Audio`
+        // if you want to see the path info replace
+        // `TypeRegistration::get_short_name` with `String::from`
+        .map(|info| TypeRegistration::get_short_name(info.name()))
+        .collect();
+
+    // sort list alphebetically
+    r.sort();
+    r.iter().for_each(|name| println!("{}", name));
+}
+// ANCHOR_END: example
+
+fn main() {
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(print_resources.system())
+        .run();
+}
