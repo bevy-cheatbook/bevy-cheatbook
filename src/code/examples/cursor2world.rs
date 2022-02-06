83edcfb7b02cfe4e6c9bdb2833929e671c7b6eba
hazelsparrow <gvorobjev@gmail.com>
2022-02-06 10:23:37 -0500

    make ortho component optional in the query

--- src/code/examples/cursor2world.rs
+++ src/code/examples/cursor2world.rs
@@ -15,7 +15,7 @@ fn my_cursor_system(
     // need to get window dimensions
     wnds: Res<Windows>,
     // query to get camera transform
-    q_camera: Query<(&Transform, &OrthographicProjection), With<MainCamera>>
+    q_camera: Query<(&Transform, Option<&OrthographicProjection>), With<MainCamera>>
 ) {
     // get the primary window
     let wnd = wnds.get_primary().unwrap();
@@ -32,8 +32,13 @@ fn my_cursor_system(
         // assuming there is exactly one main camera entity, so this is OK
         let (camera_transform, ortho_projection) = q_camera.single();
 
+        let mut scale = 1.0;
+        if let Some(ortho_projection) = ortho_projection {
+            scale /= ortho_projection.scale;
+        }
+
         // undo orthographic scale and apply the camera transform
-        let pos_wld = camera_transform.compute_matrix() * p.extend(0.0).extend(1.0 / ortho_projection.scale);
+        let pos_wld = camera_transform.compute_matrix() * p.extend(0.0).extend(1.0 / scale);
         eprintln!("World coords: {}/{}", pos_wld.x, pos_wld.y);
     }
 }
