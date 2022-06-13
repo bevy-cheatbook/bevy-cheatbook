ad345c8a258cbf915f8afc4e0c86e99b29953fe9
Ida Iyes <identry@protonmail.com>
2022-06-13 15:06:19 +0300

    update nagbar ad

--- nagbar.js
+++ nagbar.js
@@ -6,6 +6,6 @@ p1.appendChild(document.createTextNode(' Donate to help me work on this book. Th
 p1.insertAdjacentHTML('afterbegin', '<a class="github-button" href="https://github.com/sponsors/inodentry" data-icon="octicon-heart" data-size="small" aria-label="Sponsor @inodentry on GitHub">GitHub Sponsors</a>');
 nagbar.appendChild(p1);
 var p2 = document.createElement('p');
-p2.insertAdjacentHTML('afterbegin', 'I offer professional Bevy tutoring and consulting services. <a href="/contact.html">Contact me</a> if interested!');
+p2.insertAdjacentHTML('afterbegin', 'I can offer private help with Bevy (tutoring, help with your projects, consulting, etc.). <a href="/contact.html">Contact me</a> if interested!');
 nagbar.appendChild(p2);
 document.getElementById('page-wrapper').appendChild(nagbar);
