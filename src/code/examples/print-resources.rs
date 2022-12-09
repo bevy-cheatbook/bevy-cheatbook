a2e0ea48f1e0ffaadf0003f0f7da856726206c58
Ida Iyes <identry@protonmail.com>
2022-12-09 20:15:49 +0200

    update print-resources example with fix from bevy 0.9.1

--- src/code/examples/print-resources.rs
+++ src/code/examples/print-resources.rs
@@ -1,26 +1,42 @@
+use bevy::app::AppExit;
 use bevy::ecs::{archetype::Archetypes, component::Components};
 use bevy::prelude::*;
+use bevy::render::{RenderApp, RenderStage};
 
 // ANCHOR: example
-fn print_resources(archetypes: &Archetypes, components: &Components) {
-    // TODO FIX THIS WHEN BEVY 0.9.1 IS RELEASED
+fn print_resources(world: &World) {
+    let components = world.components();
 
-    // let mut r: Vec<_> = archetypes
-    //     .resource()
-    //     .components()
-    //     .map(|id| components.get_info(id).unwrap())
-    //     .map(|info| info.name())
-    //     .collect();
+    let mut r: Vec<_> = world
+        .storages()
+        .resources
+        .iter()
+        .map(|(id, _)| components.get_info(id).unwrap())
+        .map(|info| info.name())
+        .collect();
 
-    // // sort list alphebetically
-    // r.sort();
-    // r.iter().for_each(|name| println!("{}", name));
+    // sort list alphebetically
+    r.sort();
+    r.iter().for_each(|name| println!("{}", name));
 }
 // ANCHOR_END: example
 
+fn quit(mut evw: EventWriter<AppExit>) {
+    evw.send(AppExit);
+}
+
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_startup_system(print_resources)
-        .run();
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins);
+
+    // print main world resources
+    app.add_system_to_stage(CoreStage::Last, print_resources);
+
+    // print render world resources
+    app.sub_app_mut(RenderApp)
+        .add_system_to_stage(RenderStage::Render, print_resources);
+
+    app.add_system_to_stage(CoreStage::Last, quit);
+
+    app.run();
 }
