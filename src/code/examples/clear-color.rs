14954d3845314aa7f29121a037e6324a3517fba7
Ida Iyes <identry@protonmail.com>
2023-02-26 14:02:42 +0200

    clear color example: comment rewording

--- src/code/examples/clear-color.rs
+++ src/code/examples/clear-color.rs
@@ -7,15 +7,17 @@ fn setup_camera_3d(
 // ANCHOR: camera
 use bevy::core_pipeline::clear_color::ClearColorConfig;
 
-// set the color for a specific camera (3D)
+// configure the background color (if any), for a specific camera (3D)
 commands.spawn(Camera3dBundle {
     camera_3d: Camera3d {
+        // clear the whole viewport with the given color
         clear_color: ClearColorConfig::Custom(Color::rgb(0.8, 0.4, 0.2)),
         ..Default::default()
     },
     ..Default::default()
 });
-// set the color for a specific camera (2D)
+
+// configure the background color (if any), for a specific camera (2D)
 commands.spawn(Camera2dBundle {
     camera_2d: Camera2d {
         // disable clearing completely (pixels stay as they are)
